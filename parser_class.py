39e1d7e3e012cdea232ce39679dbb385685e3a85
Max <maxk893@gmail.com>
2017-08-10 12:57:09 -0700

    Update parser_class.py

--- parser_class.py
+++ parser_class.py
@@ -4,10 +4,11 @@ import xml.etree.ElementTree as ET
 class Xml_Parser(object):
 
 
-    def parser_method(self, xmlCont):
+    def parser_method(self, xmlCont, fileName):
 
         tree = ET.fromstring(str(xmlCont))
 
+        # The code below would need to be swapped for something specific to what your looking for. Left here as an example.
         for child in tree.findall('Abbr'):
             sendMode = child.find('./SendConfiguration/AddressInfo/SendMode')
             if(sendMode.text == "Fax"):
@@ -15,8 +16,8 @@ class Xml_Parser(object):
                 entryName = child.find('Name').text
                 faxNumber = child.find('./SendConfiguration/AddressInfo/FaxMode/PhoneNumber')
 
-                outputFile = open('result_1', 'a')
-                outputFile.write("%s: %s , Type: %s , Number: %s \n" % (entryNum, entryName, sendMode.text, faxNumber.text))
+                outputFile = open('result - %s' % fileName, 'a')
+                outputFile.write("%s: %s , Type: %s , Number: %s  \n\n" % (entryNum, entryName, sendMode.text, faxNumber.text))
                 outputFile.close()
 
-                #print("%s: %s , %s , Number: %s" % (entryNum, entryName, sendMode.text, faxNumber.text))
+        print("Parsed " + fileName)   
